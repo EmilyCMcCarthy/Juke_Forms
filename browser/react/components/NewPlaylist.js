42051bc2964bfff6bc58c097ffeb773e78b8e5de
Emily McCarthy <emccarthy510@yahoo.com>
2017-07-06 18:02:15 -0400

    3 NewPlaylist New Playlist State

---
+++ browser/react/components/NewPlaylist.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export default class NewPlaylist extends Component {
+
+    constructor () {
+    super();
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+  handleChange (event) {
+         this.setState({
+          inputValue: event.target.value
+        });
+    }
+
+    handleSubmit (event){
+        console.log(this.state.inputValue)
+        event.preventDefault();
+    }
+
+
+  render () {
+
+      //const artists = this.state.artists.filter(artist => artist.name.match(this.state.inputValue));
+
+      return (
+          <div className="well">
+            <form className="form-horizontal" onSubmit={this.handleSubmit}>
+                <fieldset>
+                <legend>New Playlist</legend>
+                <div className="form-group">
+                    <label className="col-xs-2 control-label">Name</label>
+                    <div className="col-xs-10">
+                    <input className="form-control" type="text" onChange={this.handleChange} />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-xs-10 col-xs-offset-2">
+                    <button type="submit" className="btn btn-success">Create Playlist</button>
+                    </div>
+                </div>
+                </fieldset>
+            </form>
+        </div>
+      
+    );
+  }
+}
